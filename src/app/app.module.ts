ba61d13c5846280c91fc230e8254bf05c666cd2c
jayachandra.goteti <jayachandra.goteti@thecollective.energy>
2023-01-26 12:18:50 +0530

    products list updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ProductsListComponent } from './products-list/products-list.component';
-import { HomeComponent } from './home/home.component';
-import { SharedComponent } from './shared/shared.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ProductsListComponent } from "./products-list/products-list.component";
+import { HomeComponent } from "./home/home.component";
+import { SharedComponent } from "./shared/shared.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsListComponent,
     HomeComponent,
-    SharedComponent
+    SharedComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
